f7bec535946ca3ae08403ed71c01deecab311187
Samuel Wang <samuel.wang21@imperial.ac.uk>
2024-11-21 12:06:22 +0000

    feat: complete task 5 with updates to task 4

--- FullerWang.thy
+++ FullerWang.thy
@@ -150,7 +150,7 @@ using sum10_digits10_not_inverse_helper by blast
 section \<open> Task 4: A divisibility theorem. \<close>
 
 text \<open> Any 6-digit number of the form ABABAB is divisible by 37 (helper lemma). \<close>
-lemma pat_ababab_is_37_divisible_helper: "(ds = digits10 n \<and> ds = [a, b, a, b, a, b]) \<Longrightarrow> n mod 37 = 0"
+lemma pat_ababab_is_37_divisible_helper: "(ds = digits10 n \<and> ds = [a, b, a, b, a, b]) \<Longrightarrow> (37 dvd n)"
 proof -
   assume"(ds = digits10 n \<and> ds = [a, b, a, b, a, b])"
   hence "n = (sum10 ds)" using digits10_sum10_inverse by presburger
@@ -161,7 +161,7 @@ proof -
   hence "10 * sum10 [b, 0, b, 0, b] mod 37 = 0" by simp
   hence "(10 * sum10 [b, 0, b, 0, b] + sum10 [a, 0, a, 0, a]) mod 37 = 0" by auto
   hence "n mod 37 = 0" using calculation by force
-  show "n mod 37 = 0" using \<open>n mod 37 = 0\<close> by blast
+  show "37 dvd n" using \<open>n mod 37 = 0\<close> by blast
 qed
 
 text \<open> Any 6-digit number of the form ABABAB is divisible by 37. \<close>
@@ -169,7 +169,7 @@ theorem pat_ababab_is_37_divisible:
   "((ds = digits10 n) \<and> (length ds = 6) \<and>
   ((hd ds) = (hd (tl (tl ds)))) \<and> ((hd ds) = (hd (tl (tl (tl (tl ds)))))) \<and>
   ((hd (tl ds)) = (hd (tl (tl (tl ds))))) \<and> ((hd (tl ds)) = (hd (tl (tl (tl (tl (tl ds))))))))
-  \<Longrightarrow> (n mod 37 = 0)"
+  \<Longrightarrow> (37 dvd n)"
 by (metis One_nat_def diff_Suc_1 eval_nat_numeral(3) length_greater_0_conv length_tl list.collapse list.size(3) numeral_eq_Suc numeral_plus_numeral pat_ababab_is_37_divisible_helper plus_1_eq_Suc pred_numeral_simps(1) pred_numeral_simps(2) semiring_norm(2) semiring_norm(28) semiring_norm(4) zero_neq_numeral)
 
 
